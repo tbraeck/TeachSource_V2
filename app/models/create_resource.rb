34c51d32427e8515f7291218fe6af0072c0e2a3c
Tate Braeckel <90567271+tbraeck@users.noreply.github.com>
2024-01-21 21:04:17 -0700

    update database, change names, try to have data on seed

--- app/models/create_resource.rb
+++
@@ -1,2 +0,0 @@
-class CreateResource < ApplicationRecord
-end
--- db/migrate/20240122034930_create_subjects.rb
+++ db/migrate/20240122034930_create_subjects.rb
@@ -1,6 +1,5 @@
 class CreateSubjects < ActiveRecord::Migration[7.1]
   def change
-
     create_table :subjects do |t|
       t.string :name
       t.string :description
